08bfd0744e1fd60fb0683df5cbc2e4a5e581e81e
meluhrose <milvas04178@stud.noroff.no>
2026-06-21 23:16:06 +0200

    Added auth/login and installed JWT
    Added login function that validates email/passwords, verifies passwords with bcrypt and issues a JWT with a 1hr expiration.

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { register } from "../controllers/authControllers";
+import { register, login } from "../controllers/authControllers";
 
 const router = Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
